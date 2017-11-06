b89399eba1e11614b9508a3cc6b10cd61ce03909
Raimon Esteve <raimon@nan-tic.com>
2017-11-06 14:04:31 +0100

    Update planned/effective date with py-sql task-020150

--- shipment.py
+++ shipment.py
@@ -1,14 +1,29 @@
 # This file is part of the stock_update_planned_date module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from datetime import datetime
 from trytond.pool import Pool, PoolMeta
 from trytond.model import ModelView, fields
 from trytond.transaction import Transaction
 from trytond.wizard import Wizard, StateView, StateTransition, Button
+from sql.operators import Or
+
+__all__ = ['Configuration', 'Move', 'UpdatePlannedDateStart',
+    'UpdatePlannedDate']
 
 
-__all__ = ['Move', 'ShipmentOut', 'UpdatePlannedDateStart',
-    'UpdatePlannedDate', 'StockConfiguration']
+class Configuration:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.configuration'
+    update_shipment_out = fields.Boolean('Update Planned Dates of '
+        'Customer Shipments')
+    update_shipment_in = fields.Boolean('Update Planned Dates of '
+        'Supplier Shipments')
+    update_shipment_internal = fields.Boolean('Update Planned Dates of '
+        'Internal Shipments')
+    update_move_out = fields.Boolean('Update Planned Dates of Sales Moves')
+    update_move_in = fields.Boolean('Update Planned Dates of Purchase Moves')
+    update_move_shipment = fields.Boolean('Update Planned of Dates Moves')
 
 
 class Move:
@@ -16,96 +31,128 @@ class Move:
     __name__ = 'stock.move'
 
     @classmethod
-    def renew_planned_date(cls, args=None, date=None):
-        pool = Pool()
+    def renew_move_planned_date(cls, origins, date=None):
         Date_ = Pool().get('ir.date')
-        Period = pool.get('stock.period')
+
+        move = cls.__table__()
+        cursor = Transaction().connection.cursor()
 
         if not date:
             date = Date_.today()
 
-        domain = [
-            ('state', 'in', ['draft', 'assigned']),
-            ('planned_date', '<', date),
-            ]
-        if args and 'shipment' in args:
-            domain.append(
-                ('shipment', 'like', args + ',%'),
-                )
-        elif args and ('purchase.line' in args or 'sale.line' in args):
-            domain.append(
-                ('origin', 'like', args + ',%'),
-                )
+        origs = [move.shipment.like(o+',%') for o in origins]
 
-        periods = Period.search([
-            ('state', '=', 'closed'),
-            ], order=[('date', 'DESC')], limit=1)
-        if periods:
-            period, = periods
-            domain.append(
-                ('planned_date', '>', period.date),
-                )
-        with Transaction().set_user(1):
-            moves = cls.search(domain)
-            if moves:
-                cls.write(moves, {'planned_date': date})
+        for field in ['planned_date', 'effective_date']:
+            sql_where = (
+                ~move.state.in_(['cancel', 'done']) & (getattr(move, field) < date))
+            if origs:
+                sql_where.append(Or(origs))
+            # move.select(move.id, where=sql_where)
+            cursor.execute(*move.update(
+                    columns=[getattr(move, field), move.write_date],
+                    values=[date, datetime.now()],
+                    where=sql_where))
 
     @classmethod
-    def update_configured_planned_date(cls):
-        'Update planned date of move in and shipment out as configured'
+    def renew_shipment_planned_date(cls, shipment_types, date=None):
+        'Renew planned date and effective date'
         pool = Pool()
+        Date_ = Pool().get('ir.date')
         ShipmentOut = pool.get('stock.shipment.out')
-        Date = pool.get('ir.date')
-        Configuration = pool.get('stock.configuration')
-        configuration = Configuration(1)
-        today = Date.today()
+        ShipmentOutReturn = pool.get('stock.shipment.out.return')
+        ShipmentIn = pool.get('stock.shipment.in')
+        ShipmentInReturn = pool.get('stock.shipment.in.return')
+        ShipmentInternal = pool.get('stock.shipment.internal')
 
-        if configuration.update_move_in:
-            cls.renew_planned_date(args='purchase.line', date=today)
-        if configuration.update_shipment_out:
-            ShipmentOut.update_planned_date()
-
-
-class ShipmentOut:
-    __metaclass__ = PoolMeta
-    __name__ = 'stock.shipment.out'
-
-    @classmethod
-    def renew_planned_date(cls, date=None):
-        'Renew planned date'
-        pool = Pool()
-        Date_ = Pool().get('ir.date')
-        Period = pool.get('stock.period')
-        Move = pool.get('stock.move')
+        shipment_out = ShipmentOut.__table__()
+        shipment_out_return = ShipmentOutReturn.__table__()
+        shipment_in = ShipmentIn.__table__()
+        shipment_in_return = ShipmentInReturn.__table__()
+        shipment_internal = ShipmentInternal.__table__()
+        cursor = Transaction().connection.cursor()
 
         if not date:
             date = Date_.today()
 
-        periods = Period.search([
-            ('state', '=', 'closed'),
-            ], order=[('date', 'DESC')], limit=1)
+        # shipment out
+        if 'stock.shipment.out' in shipment_types:
+            for shipment_th in [shipment_out, shipment_out_return]:
+                for field in ['planned_date', 'effective_date']:
+                    sql_where = (
+                        ~shipment_th.state.in_(['cancel', 'done']) & (
+                            getattr(shipment_th, field) < date))
+                    cursor.execute(*shipment_th.update(
+                            columns=[
+                                getattr(shipment_th, field),
+                                getattr(shipment_th, 'write_date'),
+                                ],
+                            values=[date, datetime.now()],
+                            where=sql_where))
 
-        # search shipment out
-        domain = [
-            ('state', 'in', ['draft', 'waiting', 'assigned']),
-            ('planned_date', '<', date),
-            ]
-        if periods:
-            period, = periods
-            domain.append(
-                ('planned_date', '>', period.date),
-                )
-        shipments = cls.search(domain)
-        if shipments:
-            cls.write(shipments, {'planned_date': date})
-        Move.renew_planned_date(cls.__name__, date=date)
+        # shipment in
+        if 'stock.shipment.in' in shipment_types:
+            for shipment_th in [shipment_in, shipment_in_return]:
+                for field in ['planned_date', 'effective_date']:
+                    sql_where = (
+                        ~shipment_th.state.in_(['cancel', 'done']) & (
+                            getattr(shipment_th, field) < date))
+                    cursor.execute(*shipment_th.update(
+                            columns=[
+                                getattr(shipment_th, field),
+                                getattr(shipment_th, 'write_date'),
+                                ],
+                            values=[date, datetime.now()],
+                            where=sql_where))
+
+        # shipment internal
+        if 'stock.shipment.internal' in shipment_types:
+            for field in ['planned_date', 'effective_date']:
+                sql_where = (
+                    ~shipment_internal.state.in_(['cancel', 'done']) & (
+                        getattr(shipment_internal, field) < date))
+                cursor.execute(*shipment_internal.update(
+                        columns=[
+                            getattr(shipment_internal, field),
+                            getattr(shipment_internal, 'write_date'),
+                            ],
+                        values=[date, datetime.now()],
+                        where=sql_where))
 
     @classmethod
-    def update_planned_date(cls, args=None):
-        'Update planned date'
-        Date = Pool().get('ir.date')
+    def update_planned_date(cls, date=None):
+        'Update planned date of moves and shipments as configured'
+        pool = Pool()
+        Date_ = pool.get('ir.date')
+        Configuration = pool.get('stock.configuration')
 
-        cls.renew_planned_date(Date.today())
+        conf = Configuration(1)
+        if not date:
+            date = Date_.today()
+
+        # stock.shipment
+        shipment_types =[]
+        if conf.update_shipment_out:
+            shipment_types.append('stock.shipment.out')
+        if conf.update_shipment_in:
+            shipment_types.append('stock.shipment.in')
+        if conf.update_shipment_internal:
+            shipment_types.append('stock.shipment.internal')
+        if shipment_types:
+            cls.renew_shipment_planned_date(shipment_types, date=date)
+
+        # stock.move
+        origins = []
+        if conf.update_move_in:
+            origins.append('purchase.line')
+        if conf.update_move_out:
+            origins.append('sale.line')
+        if conf.update_move_shipment:
+            origins.append('stock.shipment.out')
+            origins.append('stock.shipment.out.return')
+            origins.append('stock.shipment.in')
+            origins.append('stock.shipment.in.return')
+            origins.append('stock.shipment.internal')
+        cls.renew_move_planned_date(origins, date=date)
 
 
 class UpdatePlannedDateStart(ModelView):
@@ -130,17 +177,7 @@ class UpdatePlannedDate(Wizard):
     update_planned_date = StateTransition()
 
     def transition_update_planned_date(self):
-        ShipmentOut = Pool().get('stock.shipment.out')
+        Move = Pool().get('stock.move')
 
-        ShipmentOut.renew_planned_date(self.start.date)
+        Move.update_planned_date(self.start.date)
         return 'end'
-
-
-class StockConfiguration:
-    __metaclass__ = PoolMeta
-    __name__ = 'stock.configuration'
-
-    update_shipment_out = fields.Boolean('Update Planned Dates of Customer '
-        'Shipments')
-    update_move_in = fields.Boolean('Update Planned Dates of Purchase '
-        'Movements')
