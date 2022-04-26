0bbd5ff75d5835e1f97ceb574a80a5d862c8544d
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:09 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part of the stock_update_planned_date module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.stock_update_planned_date.tests.test_stock_update_planned_date import suite
-except ImportError:
-    from .test_stock_update_planned_date import suite
-
-__all__ = ['suite']
